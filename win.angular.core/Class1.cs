c7cb04b3e2bed46e9100d390189e0a75d0cfc3bc
Fernando Cararo dos Reis <fernandocararo@outlook.com>
2020-07-28 20:40:15 -0300

    feat: init.

---
+++ win.angular.core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace win.angular.core
+{
+    public class Class1
+    {
+    }
+}
